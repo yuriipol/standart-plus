4a13aaa1c75475ef53c2a6f22f5cf2a3117893f3
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-06-15 16:02:31 +0300

    add reac-icons and shpopping icon

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import s from "./Header.module.scss";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
       <div>
         <span className={s.logo}>Standart +</span>
+
         <ul className={s.nav}>
           <li className={s.nav_item}>Про нас</li>
           <li className={s.nav_item}>Контакти</li>
           <li className={s.nav_item}>Кабінет</li>
         </ul>
+        <FaShoppingCart className={s.shoppingCartButton} />
       </div>
       <div className={s.baner}></div>
     </header>

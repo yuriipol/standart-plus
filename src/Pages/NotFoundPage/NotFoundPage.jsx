bc2db82aa8b117531a9d6c04b74c4e08159279a0
unknown <polupan85@gmail.com>
2023-07-02 10:16:48 +0300

    add style and image to page notFound

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import NotPage from "../../Shared/Images/404.jpg"
 import s from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <h1 className={s.title}>Page NOT FOUND!</h1>
+      {/* <h1 className={s.title}>Page NOT FOUND!</h1> */}
       <button type="button" className={s.goBack} onClick={() => navigate(-1)}>
         Go Back
       </button>
+    <img src={NotPage} alt="404" className={s.notFound}/>
+      
     </div>
   );
 };
